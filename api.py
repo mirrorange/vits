53233f36f218466cfc3702c6f5e80b3b52ac8d80
orange <orange@icedeer.net>
2022-10-10 23:24:01 +0800

    确保模型存在

--- api.py
+++ api.py
@@ -1,21 +1,25 @@
-from crypt import methods
-from pyexpat import model
 from time import time
 from flask import Flask, request,send_file
 from inference import vits_inference
 import os
-import time
-from threading import Timer
+
+class ModelNotFoundError(RuntimeError):
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.message = f"Model [ {self.model_name} ] Not Found!"
 
 def load_model(model_name):
     if not model_name in model_list:
-        model_list[model_name] = vits_inference(model_name)
+        raise ModelNotFoundError(model_name)
+    if not model_name in models:
+        models[model_name] = vits_inference(model_name)
 
 def release_model(model_name):
-    if not model_name in model_list:
-        del model_list[model_name]
+    if model_name in models:
+        models.pop(model_name)
 
-model_list = {}
+model_list = ["paimon","atri","ljs"]
+models = {}
 
 app = Flask(__name__)
 
@@ -28,11 +32,16 @@ def synthesis():
     model_name = request.args.get('model_name',type=str)
     target_text = request.args.get('target_text',type=str)
     speaker_id = request.args.get('speaker_id',default=-1,type=int)
-    output_file = os.path.join("output",str(time.time()) + ".wav")
-    if not model_name in model_list:
-        load_model(model_name)
+    output_file = os.path.join("output",str(time()) + ".wav")
+    if not model_name in models:
+        try:
+            load_model(model_name)
+        except ModelNotFoundError as e:
+            return e.message
+        except:
+            return "Model loading failed"
     try:
-        model_list[model_name].synthesis(output_file,target_text,speaker_id)
+        models[model_name].synthesis(output_file,target_text,speaker_id)
         return send_file(output_file)
     except:
         return "Synthesis failed"
@@ -43,6 +52,8 @@ def load():
     try:
         load_model(model_name)
         return "Model Loaded"
+    except ModelNotFoundError as e:
+        return e.message
     except:
         return "Model loading failed"
 
